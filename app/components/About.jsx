80ac0b1bdfc4d557de1cf1b6ab01ed07e8a3b8c7
Hector Manuel Valdivia Renteria <Hector Manuel Valdivia Renteria>
2016-09-06 01:10:11 -0600

    Style About

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,14 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <div>
-      <h3>About Component</h3>
+    <div className="row">
+      <h1 className="text-center">About Component</h1>
+      <p>Listo no tengo ganas odio fundation</p>
+      <ul>
+        <li>Se uso React</li>
+        <li>Se uso openweather</li>
+        <li>Se uso fundation</li>
+      </ul>
     </div>
   );
 };
